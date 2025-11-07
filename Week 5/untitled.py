79a1419841257bf226a5bc0f3536f76b3cbd7bcf
mariarty9 <borysenko.mr31@gmail.com>
2025-11-07 15:21:05 +0100

    Week 5
    Theory

---
+++ Week 5/untitled.py
@@ -0,0 +1,11 @@
+def hello():
+    """Prints a friendly greeting."""
+    print('Hi')
+    
+def goodbye():
+    """Prints a farewell message."""
+    print('Bye')
+    
+def greet(name):
+    """Greets a person by name."""
+    return f'Hello, {name}!'
